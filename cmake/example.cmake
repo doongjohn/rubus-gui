8905cdd3a9b9ca933addcdd2ed22bb47bb307f7b
doongjohn <doongjohn@gmail.com>
2024-05-19 15:23:55 +0900

    update

--- cmake/example.cmake
+++ cmake/example.cmake
@@ -1,18 +1,8 @@
 add_executable(rubus-gui-example "")
-
 set_property(TARGET rubus-gui-example PROPERTY EXCLUDE_FROM_ALL true)
 set_property(TARGET rubus-gui-example PROPERTY CXX_STANDARD 20)
-
-set(USE_ASAN false CACHE BOOL "Use AddressSanitizer")
-if (USE_ASAN)
-  set(CMAKE_CXX_FLAGS_DEBUG "-O1")
-  set_property(TARGET rubus-gui-example PROPERTY MSVC_RUNTIME_LIBRARY MultiThreaded)
-  set(SANITIZER_OPTS -fno-omit-frame-pointer -fno-sanitize-recover=all -fsanitize=address,undefined)
-  target_compile_options(rubus-gui-example PRIVATE ${SANITIZER_OPTS})
-  target_link_options(rubus-gui-example PRIVATE ${SANITIZER_OPTS})
-else()
-  set_property(TARGET rubus-gui-example PROPERTY MSVC_RUNTIME_LIBRARY MultiThreaded$<$<CONFIG:Debug>:Debug>)
-endif()
+set_property(TARGET rubus-gui-example PROPERTY MSVC_RUNTIME_LIBRARY MultiThreaded$<$<CONFIG:Debug>:Debug>)
+use_sanitizer(rubus-gui-example)
 
 target_sources(
   rubus-gui-example
@@ -36,15 +26,7 @@ target_link_libraries(
 
 add_subdirectory("example/vendor/glfw-3.4")
 set_property(TARGET glfw PROPERTY EXCLUDE_FROM_ALL true)
-
-if (USE_ASAN)
-  set(CMAKE_CXX_FLAGS_DEBUG "-O1")
-  set_property(TARGET glfw PROPERTY MSVC_RUNTIME_LIBRARY MultiThreaded)
-  set(SANITIZER_OPTS -fno-omit-frame-pointer -fno-sanitize-recover=all -fsanitize=address,undefined)
-  target_compile_options(glfw PRIVATE ${SANITIZER_OPTS})
-  target_link_options(glfw PRIVATE ${SANITIZER_OPTS})
-else()
-  set_property(TARGET glfw PROPERTY MSVC_RUNTIME_LIBRARY MultiThreaded$<$<CONFIG:Debug>:Debug>)
-endif()
+set_property(TARGET glfw PROPERTY MSVC_RUNTIME_LIBRARY MultiThreaded$<$<CONFIG:Debug>:Debug>)
+use_sanitizer(glfw)
 
 skia_copy_icudtl_dat(rubus-gui-example)
